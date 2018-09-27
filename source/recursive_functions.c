af99d4451b043b4fe3aa81809173ee399e45da10
vkremneva <snowst@bk.ru>
2018-09-27 12:16:46 +0300

    the types of an arguments and returning values are changed

--- source/recursive_functions.c
+++ source/recursive_functions.c
@@ -1,27 +1,20 @@
 #include "recursive_functions.h"
 
-extern bool Z(uint32_t x, ... ) {return 0;}
+extern bool Z(int64_t x) {return 0;}
 
-extern int64_t S(uint32_t x) {return x+1;}
+extern int64_t S(int64_t x) {return x+1;}
 
-extern int64_t P(uint32_t k, uint32_t m, ... ) {
-	if (k < m) {
-		int64_t result = -1;
-
-		va_list argptr;
-		va_start(argptr, m);
-		for (int64_t i = 0; i < k; i++)
-			result = va_arg(argptr, int64_t);
-
-		va_end(argptr);
-		
-		if (result == -1) {
-			fprintf(stderr, "Incorrect argument: no 'x' arguments in P[k, m](x1, ...) function\n");
-            _Exit(1);
-		}
-		return result;
-	} else {
+extern int64_t P(int64_t k, int64_t m, int64_t *args) {
+	if (k > m) {
 		fprintf(stderr, "Incorrect argument: in P[k, m] k shoul be less than m\n");
 		_Exit(1);
 	}
+
+	if (sizeof(args) < m) {
+		fprintf(stderr, "Incorrect arguments: in P[k, m](x1, ..., xt) m = t\n"
+				  "Here m = %lld, t=%i", m, sizeof(args));
+		_Exit(1);
+	}
+
+	return args[k-1];
 }
--- source/recursive_functions.h
+++ source/recursive_functions.h
@@ -8,12 +8,12 @@
 
 //z() is a null-function
 //returns zero in any case
-extern bool Z(uint32_t x, ... );
+extern bool Z(int64_t x);
 
 //s() is a successor function
 //returns x+1
-extern int64_t S(uint32_t x);
+extern int64_t S(int64_t x);
 
 //identify function P[k, m](x1, x2, x3, ..., xm)
 //returns xk
-extern int64_t P(uint32_t k, uint32_t m, ... );
+extern int64_t P(int64_t k, int64_t m, int64_t *args);
