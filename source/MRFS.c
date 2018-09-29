114284e48e3339a7f692d721437c4dc726f6913a
vkremneva <snowst@bk.ru>
2018-09-29 18:42:05 +0300

    structure changes + simple tests

--- source/MRFS.c
+++ source/MRFS.c
@@ -1,10 +1,10 @@
-//#include<string.h>
+#include<string.h>
+#include <assert.h>
 //#include <ctype.h> //classify and convert single characters
 
-#include "basis.h"
-#include "function.h"
+#include "evaluate.h"
 
-int main(int argc, char *argv[]) {
+int main0(int argc, char *argv[]) {
 
 	bool err = false;
 	
@@ -27,4 +27,54 @@ int main(int argc, char *argv[]) {
 	fclose(finput);
 	
 	return 0;
+}
+
+void test_Z() {
+	struct Function s;
+	s.is_primitive = true;
+	strcpy(s.name, "Z");
+	s.left = 0;
+	s.right = 0;
+	struct Arguments arg;
+	arg.arity = 2;
+	arg.args[0] = 4, arg.args[1] = 2;
+	evaluate(s, arg);
+
+	assert(evaluate(s, arg) == 0);
+
+    arg.arity = 0;
+    assert(evaluate(s, arg) == 0);
+}
+
+void test_S() {
+    struct Function s;
+    s.is_primitive = true;
+    strcpy(s.name, "S");
+
+    struct Arguments arg;
+    arg.arity = 0;
+
+    for (int i = 0; i < 10; ++i) {
+        arg.args[0] = i;
+        assert(evaluate(s, arg) == i + 1);
+    }
+}
+
+void test_P() {
+
+}
+
+int main() {
+	test_Z();
+	test_S();
+	test_P();
+	return 0;
+}
+
+void test_1() {
+	struct Function func1;
+	func1.is_primitive = false;
+	strcpy(func1.name, "func_1");
+	strcpy(func1.operator, "O");
+	func1.left = 0;
 }
